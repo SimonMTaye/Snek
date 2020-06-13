2b52f79ea14dd27d6104fddcf5788cf42904d13d
SirPotato10000 <mulat.simon@gmail.com>
2020-06-13 21:43:21 +0300

    Created GameManager
    Responsible for co-ordinating SpawnManager and Player

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject playerPrefab;
+    private SpawnManager spawnManager;
+    public float blockSpawnDelay = 10f;
+    private float blockTimer = 0f;
+
+    public enum Difficulties
+    {
+        EASY,
+        MEDIUM,
+        HARD,
+        VERY_HARD
+    }
+    private Difficulties _difficulty;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spawnManager = GameObject.Find("SpawnManager").GetComponent<SpawnManager>();
+        spawnManager.SpawnPellet();
+        spawnManager.SpawnBlock();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        blockTimer += Time.deltaTime;
+        if (blockTimer >= blockSpawnDelay){
+            blockTimer = 0;
+            spawnManager.SpawnBlock();
+        }
+    }
+
+    public void SetDiffculty(Difficulties difficulty){
+        this._difficulty = difficulty;
+    }
+    public void PelletCollected(){
+        spawnManager.SpawnPellet();
+    }
+    IEnumerator PelletSpawner(){
+        yield return new WaitForSeconds(5f);
+        spawnManager.SpawnPellet();
+    }
+    public void GameOver(){
+        Time.timeScale = 0;
+    }
+}
