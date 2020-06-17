fc1842118e7bef7e8eda471c410b58d3e3a76be4
SirPotato10000 <mulat.simon@gmail.com>
2020-06-17 13:08:47 +0300

    Adjusted grid sizes

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,25 +13,27 @@ public class GridManager : MonoBehaviour
     public Vector3 gridOrigin;
     //Determines how many sprites a single world unit can bold.
     //Settings this to a larger number will decrease the size of the sprites (increases the number of grid sqaures)
-    public int SpritesToWorldUnit;
+    private float spritesToWorldUnit;
+    private int gridSize;
     private void Awake()
     {
-        SpritesToWorldUnit = PlayerPrefs.GetInt("Grid Size", 2);
+        gridSize = PlayerPrefs.GetInt("Grid Size", 2);
+        spritesToWorldUnit = 1f + (0.5f * gridSize);
         InitGrid();
     }
     private void InitGrid()
     {
         //Calculate colums, rows and scale based on screen aspect ratio
-        rows = (int)Camera.main.orthographicSize * 2 * SpritesToWorldUnit;
-        columns = Mathf.FloorToInt(rows * Camera.main.aspect );
+        rows = (int) (Camera.main.orthographicSize * 2 * spritesToWorldUnit);
+        columns = Mathf.FloorToInt((Camera.main.orthographicSize * 2 * spritesToWorldUnit) * Camera.main.aspect );
 
         blockScale = new Vector3
-            (Camera.main.aspect / (((float)columns / rows) * SpritesToWorldUnit),
-            1f / SpritesToWorldUnit, 
+            (Camera.main.aspect / (((float)columns / rows) * spritesToWorldUnit),
+            1f / spritesToWorldUnit, 
             1f);
         gridOrigin = new Vector3(
-            (-Camera.main.orthographicSize * Camera.main.aspect) - (0.5f * 1f / SpritesToWorldUnit), 
-            -Camera.main.orthographicSize - (0.5f * 1f / SpritesToWorldUnit), 
+            (-Camera.main.orthographicSize * Camera.main.aspect) - (0.5f * 1f / spritesToWorldUnit), 
+            -Camera.main.orthographicSize - (0.5f * 1f / spritesToWorldUnit), 
             1
             );
     }
