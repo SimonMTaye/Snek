3a0e4fc1ba0adfd581db77077cd5ca29fd1f2287
SirPotato10000 <mulat.simon@gmail.com>
2020-06-17 17:14:16 +0300

    Added movement through touch

--- Assets/Scripts/SnakeHead.cs
+++ Assets/Scripts/SnakeHead.cs
@@ -9,15 +9,15 @@ public class SnakeHead : SnakeSegment {
     private GameManager gameManager;
     private GridManager gridManager;
     //Used for determing movment direction and parsing user input
-    private enum Directions {
+    public enum Directions {
         UP = 1,
         DOWN = -1,
-        LEFT = 2,
-        RIGHT = -2
+        LEFT = -2,
+        RIGHT = 2
     }
-    private Directions _direction = Directions.LEFT;
+    private Directions _direction = Directions.RIGHT;
     //Used to validate movment direction
-    private Directions _prevDirection = Directions.LEFT;
+    private Directions _prevDirection = Directions.RIGHT;
     public float speed = 6.0f;
     // World bounds for wraping
     private float rightBound, upperBound;
@@ -36,9 +36,9 @@ public class SnakeHead : SnakeSegment {
     }
     private void Update() {
         //Recieve user Input to change direction
-        if(Input.GetAxis("Horizontal") > 0){
+        if(Input.GetAxis("Horizontal") < 0){
             ChangeDirection(Directions.LEFT);
-        } else if ((Input.GetAxis("Horizontal") < 0)){
+        } else if ((Input.GetAxis("Horizontal") > 0)){
             ChangeDirection(Directions.RIGHT);
         } else if((Input.GetAxis("Vertical") > 0)){
             ChangeDirection(Directions.UP);
@@ -47,25 +47,28 @@ public class SnakeHead : SnakeSegment {
         }
     }
     
-    private void ChangeDirection(Directions direction){
+    public void ChangeDirection(Directions direction){
         // * Opposite directions have negative values of each other i.e right = 1 and left = -1
-        // * This is used to validate that the direction switched too isn't to the opposite of the current direction
+        // * This is used to validate that the direction switched to isn't to the opposite of the current direction
         //Change direction for next Move call based on user input
         if( (int)direction * -1 != (int)_prevDirection ){
             _direction = direction;
         }
     }
 
+    public Directions GetDirection(){
+        return _direction;
+    }
     public IEnumerator MoveSnake() {
         yield return new WaitForSeconds(1 / speed);
         Wrap();
         Vector3 oldPos = this.transform.position;
         switch(_direction){
-            case Directions.LEFT:
+            case Directions.RIGHT:
                 this.transform.rotation = Quaternion.Euler(0, 0, 0);
                 this.transform.position += new Vector3(blockWidth, 0, 0);
                 break;
-            case Directions.RIGHT:
+            case Directions.LEFT:
                 this.transform.rotation = Quaternion.Euler(0, 0, 180);
                 this.transform.position += new Vector3(-blockWidth, 0, 0);
                 break;
---
+++ Assets/Scripts/TouchControl.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using static SnakeHead;
+
+public class TouchControl : MonoBehaviour
+{
+
+    private SnakeHead player;
+    private Vector3 fingerUpPos, fingerDownPos;
+    [SerializeField]
+    private float swipeThreshold = 30f;
+    private float touchTimer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        touchTimer = 0f;
+        player = this.gameObject.GetComponent<SnakeHead>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        touchTimer += Time.deltaTime;
+        foreach (Touch touch  in Input.touches)
+        {
+            if(touch.phase == TouchPhase.Began){
+                fingerDownPos = touch.position;
+                touchTimer = 0f;
+            }
+            if (touch.phase == TouchPhase.Ended){
+                fingerUpPos = touch.position;
+                if (touchTimer < 1f && PlayerPrefs.GetInt("Touch Control", 0) == 1)
+                {
+                    ClickMove(fingerDownPos);
+                } else if (touchTimer < 2f && PlayerPrefs.GetInt("Touch Control", 0) == 2){
+                    SwipeMove(fingerUpPos, fingerDownPos);
+                }
+            }
+        }
+    }
+    private void SwipeMove(Vector3 upPos, Vector3 downPos){
+        float xOffset = upPos.x - downPos.x;
+        float yOffset = upPos.y - downPos.y;
+        if(Mathf.Abs(xOffset) > swipeThreshold && Mathf.Abs(yOffset) < swipeThreshold){
+            if(xOffset < 0){
+                print("Going Left");
+                player.ChangeDirection(Directions.LEFT);
+            } else {
+                print("Going Right");
+                player.ChangeDirection(Directions.RIGHT);
+                return;
+            }
+        } else if (Mathf.Abs(yOffset) > swipeThreshold && Mathf.Abs(xOffset) < swipeThreshold){
+            if(yOffset < 0){
+                print("Going Down");
+                player.ChangeDirection(Directions.DOWN);
+            } else {
+                print("Going Up");
+                player.ChangeDirection(Directions.UP);
+                return;
+            }
+        }
+    }
+    private void ClickMove(Vector2 touchPos){
+        //Determine which part of the screen was clicked
+        //Adjust direction
+        
+        bool leftSideClicked = touchPos.x < (Screen.width / 2);
+        switch (player.GetDirection()){
+            case Directions.LEFT:
+                if(!leftSideClicked){
+                    player.ChangeDirection(Directions.UP);
+                } else {
+                    player.ChangeDirection(Directions.DOWN);
+                }
+                break;
+            case Directions.RIGHT:
+                if(!leftSideClicked){
+                    player.ChangeDirection(Directions.DOWN);
+                } else {
+                    player.ChangeDirection(Directions.UP);
+                }
+                break;
+            case Directions.UP:
+                if(!leftSideClicked){
+                    player.ChangeDirection(Directions.RIGHT);
+                } else {
+                    player.ChangeDirection(Directions.LEFT);
+                }
+                break;
+            case Directions.DOWN:
+                if(!leftSideClicked){
+                    player.ChangeDirection(Directions.LEFT);
+                } else {
+                    player.ChangeDirection(Directions.RIGHT);
+                }
+                break;
+            }
+
+    }
+}
